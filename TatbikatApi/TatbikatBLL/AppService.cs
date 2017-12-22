8e1edd41425cbc138e7b630c23eb036b390778a6
Shahrourit <shahrourit@gmail.com>
2017-12-23 01:15:23 +0300

    Tatbikat API

---
+++ TatbikatApi/TatbikatBLL/AppService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatbikatModels;
+
+namespace TatbikatBLL
+{
+    public class AppService
+    {
+        private TatbikatEntities db = new TatbikatEntities();
+
+        public List<App> GetAllApps()
+        {
+            return db.Apps.Where(i => i.IsVerified).ToList();
+        }
+
+        public async Task AddApp(App newApp)
+        {
+            newApp.IsVerified = false;
+            newApp.DateAdded = DateTime.UtcNow;
+            newApp.DateVerified = null;
+            db.Apps.Add(newApp);
+            await db.SaveChangesAsync();
+        }
+    }
+}
---
+++ TatbikatApi/TatbikatBLL/CategoryService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatbikatModels;
+
+namespace TatbikatBLL
+{
+    public class CategoryService
+    {
+        private TatbikatEntities db = new TatbikatEntities();
+
+        public List<Category> GetAllCategory()
+        {
+            return db.Categories.ToList();
+        }
+    }
+}
